0773adefd61223e771a0313f4a54c1683873a01b
Nicholas Drake <hellolao8n@gmail.com>
2021-01-08 01:15:31 +0000

    added longest non repeating substring passing implementation - although not optimized

---
+++ usememory/hash/applied/longest_non_repeating_substring/longest_non_repeating_substring.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // naive solution
+        // for loop of all possible starting positions and 
+        // for loop of all possible finishing positions
+        // for o(n^2) complexity
+        
+        // maintain a rolling set of values in the substring.
+        // i.e. start_index = 0, end_index = 0
+        // loop through whilst finding chars not in set
+        // if already in set then jump start from where that value was in set
+        // therefore should use a map to know where to jump to
+        Map<Character, Integer> substringMap = new HashMap<>();
+        
+        int substringStart = 0;
+        int substringFinish = 0;
+        int longestSubstring = 0;
+        while(substringFinish < s.length()){
+            char c = s.charAt(substringFinish);
+            // c already exists so we jump substringStart to where that last 
+            // instance was
+            if(substringMap.containsKey(c)){
+                int nextCIndex = substringMap.get(c);
+                while(substringStart <= nextCIndex){
+                    char k = s.charAt(substringStart);
+                    substringMap.remove(k);
+                    substringStart++;
+                }
+            }
+            else {
+                // can only possibly be longer if didn't have to remove from map
+                if(substringFinish - substringStart >= longestSubstring){
+                    longestSubstring = substringFinish - substringStart + 1;
+                }
+            }
+            substringMap.put(c, substringFinish);
+            substringFinish++;
+        }
+        return longestSubstring;
+    }
+}

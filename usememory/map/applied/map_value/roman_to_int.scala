d902ee36f5931a2efe2ac42bee8bffb082e7c49d
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2021-11-09 12:30:38 +0000

    add roman int golang implementations

--- usememory/map/applied/map_value/roman_to_int.scala
+++ usememory/map/applied/map_value/roman_to_int.scala
@@ -21,4 +21,19 @@ class Solution {
         }
         return num;
     }
+
+    def romanToInt(s: String): Int = {
+          val romanToIntMap = Map(
+            'M'-> 1000,
+            'D'-> 500,
+            'C'-> 100,
+            'L'-> 50,
+            'X'-> 10,
+            'V'-> 5,
+            'I'-> 1
+        )
+        s.map(romanToIntMap).foldLeft((0, 0)){
+            case ((sum, last), curr) => (sum + curr + (if (last < curr) - 2 * last else 0), curr)
+        }._1
+    }
 }

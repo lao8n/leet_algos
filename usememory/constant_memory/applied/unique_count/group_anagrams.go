d6383f3e8810fdb3ba7dcbc5a71fe50f5a7209db
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2021-11-17 12:50:28 +0000

    add group anagrams golang

---
+++ usememory/constant_memory/applied/unique_count/group_anagrams.go
@@ -0,0 +1,16 @@
+func groupAnagrams(strs []string) [][]string {
+	m := make(map[[26]int][]string)
+	for _, s := range strs {
+		var table [26]int
+		for _, char := range s {
+			table[char-'a']++
+		}
+		m[table] = append(m[table], s)
+	}
+
+	var res [][]string
+	for _, val := range m {
+		res = append(res, val)
+	}
+	return res
+}

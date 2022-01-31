649969dcf0b1189b4258541d563e2960fc45cbed
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2022-01-31 11:28:40 +0000

    recursion and iterative reverse linked list with accumulator

---
+++ recursion/linear_acc/applied/reverse_list.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// choice 1. look at next or look at current
+
+func reverseList(head *ListNode) *ListNode {
+	return reverseListRec(head, nil)
+}
+
+func reverseListRec(head *ListNode, reversed *ListNode) *ListNode {
+	// base case
+	if head == nil {
+		return reversed
+	}
+	// recursive case
+	next := ListNode{Val: head.Val, Next: reversed}
+	result := reverseListRec(head.Next, &next)
+	return result
+}

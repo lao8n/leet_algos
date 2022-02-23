372a9fbeeb5c2e482898f96e74155cf7f74115b9
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2022-02-23 10:52:14 +0000

    add lowest common ancestor dfs

---
+++ recursion/tree/applied/lowest_common_ancestor.go
@@ -0,0 +1,29 @@
+package leet_algos
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	// base case
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	l := lowestCommonAncestor(root.Left, p, q)
+	r := lowestCommonAncestor(root.Right, p, q)
+	// first time both branches non-nil this is common ancestor
+	if l != nil && r != nil {
+		return root
+	} else if l != nil {
+		return l
+	} else if r != nil {
+		return r
+	} else {
+		return nil
+	}
+}

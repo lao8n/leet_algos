1885d61bbc324e518d919c9da6c2f75b87308a4a
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2022-04-19 11:01:18 +0100

    add generate parenthesis - key idea is you track l and r separately

---
+++ recursion/double_linear_acc/generate_parenthesis.go
@@ -0,0 +1,32 @@
+// approach = recursive function
+// questions
+// 1. how acc solutions? in return function as already have []string
+// 2. how acc combinations? in argument
+// 3. how avoid repetition?
+// 4. problem -> how generate "(())(())" pattern
+
+// general idea - given a combination c there are 3 options
+// ()c, (c), ()c
+func generateParenthesis(n int) []string {
+	if n == 0 {
+		return []string{}
+	}
+	result := []string{}
+	generateParenthesisRecursive(n, n, "", &result)
+	return result
+}
+
+func generateParenthesisRecursive(l, r int, accString string, result *[]string) {
+	if r < l {
+		return
+	}
+	if l == 0 && r == 0 {
+		*result = append(*result, accString)
+	}
+	if l > 0 {
+		generateParenthesisRecursive(l-1, r, accString+"(", result)
+	}
+	if r > 0 {
+		generateParenthesisRecursive(l, r-1, accString+")", result)
+	}
+}

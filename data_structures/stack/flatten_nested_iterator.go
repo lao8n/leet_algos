009711624f939357f10ec096b89e3165343d80bc
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2022-09-13 12:28:41 +0100

    add stack complexities

--- data_structures/stack/flatten_nested_iterator.go
+++ data_structures/stack/flatten_nested_iterator.go
@@ -32,17 +32,22 @@ type NestedIterator struct {
 	topIndex int
 }
 
+// Space complexity = O(N + L) where we have stack being expanded list of integers and numbers
+
+// Time O(1) - no need to reverse as just maintain topIndex pointer to the front
 func Constructor(nestedList []*NestedInteger) *NestedIterator {
 	ni := &NestedIterator{
 		stack:    nestedList,
 		topIndex: 0,
 	}
-	ni.getIntegerAtTopOfStack()
+	ni.setIntegerAtTopOfStack()
 	return ni
 }
 
-func (this *NestedIterator) getIntegerAtTopOfStack() {
-	// assume len stack > 0 as has next already called
+// Time O(1) for integer or O(L/N) where L is the total number of lists and O(N).
+// where the complexity comes from O(N + L) where need to see every list and number once
+// and then amoritized this is O(N + L) / O(N) = O(1 + L/N) = O(L/N)
+func (this *NestedIterator) setIntegerAtTopOfStack() {
 	if this.topIndex >= len(this.stack) {
 		return
 	}
@@ -53,19 +58,21 @@ func (this *NestedIterator) getIntegerAtTopOfStack() {
 		this.stack = append(nestedList, this.stack[this.topIndex+1:]...)
 		// check integer is top of stack again
 		this.topIndex = 0
-		this.getIntegerAtTopOfStack()
+		this.setIntegerAtTopOfStack()
 	}
 	// if integer is top of stack work is done
 }
 
+// O(1) except if need to call setIntegerAtTopOfStack so then O(L/N)
 func (this *NestedIterator) Next() int {
 	// we have already guaranteed this is top of stack
 	topOfStack := this.stack[this.topIndex]
 	this.topIndex += 1
-	this.getIntegerAtTopOfStack()
+	this.setIntegerAtTopOfStack()
 	return topOfStack.GetInteger()
 }
 
+// O(1)
 func (this *NestedIterator) HasNext() bool {
 	return this.topIndex < len(this.stack)
 }

30691abecee3d0f40552eb4866ba72aa659fadd4
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2022-12-21 13:04:11 +0000

    a bit of cleanup and reviewed up to stack

---
+++ data_structures/priority_queue/heap.md
@@ -0,0 +1,4 @@
+heap is a tree which 
+* heap property - either max or min ordering where parent is >= or <=  children
+* implementation of a priority queue
+* it isn't a binary search tree where there is implied sequence of in-order traversal 
--- data_structures/sorted/sorted_key/merge_intervals.go
+++ data_structures/sorted/sorted_key/merge_intervals.go
@@ -5,7 +5,7 @@ import "sort"
 // approach 1 = 1. sort O(n logn) 2. merge O(n)
 // approach 2 = 1. merge sort combined
 func merge(intervals [][]int) [][]int {
-	// 1. custom sort - comparing just start index
+	// 1. sort - comparing just start index
 	sort.Slice(intervals,
 		func(i, j int) bool {
 			return intervals[i][0] < intervals[j][0]
@@ -18,7 +18,7 @@ func merge(intervals [][]int) [][]int {
 		} else {
 			// overlap
 			if mergedIntervals[len(mergedIntervals)-1][1] >= interval[0] {
-				// but some not overlapping
+				// but only update interval is it is longer
 				if interval[1] >= mergedIntervals[len(mergedIntervals)-1][1] {
 					mergedIntervals[len(mergedIntervals)-1][1] = interval[1]
 				}

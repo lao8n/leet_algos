483ba0ec78f40501807db948b4cf547155114175
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2023-02-08 12:42:18 +0000

    maximum score via recursion dp

--- dynamic_programming/dynamic_programming.md
+++ dynamic_programming/dynamic_programming.md
@@ -5,14 +5,20 @@ Types of algorithm
 
 Two approaches to Dynamic Programming
 * Bottom-up tabulation = start with the base cases and iterate up - adv: iteration faster than recursion
-* Top-down memoization = start with case you want and recurse down - adv: ordering of recursion does not matter, or if not all subproblems need to be solved
+* Top-down memoization = start with case you want and recurse down - adv: ordering of recursion does not matter, or if not all subproblems need to be solved. Memoization requires immutable key however so no slices
+
+Steps to convert top-down to bottom-up
+1. initialize memoized slice of possible states
+2. set base cases
+3. for loops from bases cases
+4. copy top-down logic to corresponding bottom up
 
 When to use Dynamic programming
 * max/min or number of ways 
 * earlier decisions affect future decisions (i.e. cannot use greedy because of counter-example)
 
 Dynamic programming checklist
-* State function/data structure to give answer to the problem for every given state
+* State function/data structure to give answer to the problem for every given state (e.g. index/indices, constraints)
 * Recurrence relation to transition between states
 * Base cases, so recursion/iteration ends
 
---
+++ recursion/dfs/return_solution/post_recursion/memoize/maximum_score.go
@@ -0,0 +1,45 @@
+package recursion
+
+// interdependent subproblems combined for a solution -> combined for a solution
+// two approaches: 1. [choose] top down recursion with memoization 2. bottom up with tabulation
+
+// need 3 things for dp
+// 1. base cases
+// 2. recurrence relation
+// 3. state definition = left index, right index, multiplier index
+// recursion two choices for solution 1. accumulator argument 2. [choose] return
+// how to memoize? need something immutable and reduce dimensions from 3 -> 2
+func maximumScore(nums []int, multipliers []int) int {
+	// m x l array - default to 0
+	memoized := make([][]int, len(multipliers))
+	for i := range memoized {
+		memoized[i] = make([]int, len(nums))
+	}
+	return maximumScoreDp(nums, multipliers, memoized, 0, 0)
+}
+
+func maximumScoreDp(nums []int, multipliers []int, memoized [][]int, m int, l int) int {
+	// don't need 3 arguments can calculate r from l & m
+	r := len(nums) - 1 - m + l
+
+	// base cases
+	if m == len(multipliers) {
+		return 0
+	}
+	// memoized case
+	if memoized[m][l] != 0 {
+		return memoized[m][l]
+	}
+
+	// recurrence cases
+	// choose left
+	left := maximumScoreDp(nums, multipliers, memoized, m+1, l+1) + nums[l]*multipliers[m]
+	// choose right
+	right := maximumScoreDp(nums, multipliers, memoized, m+1, l) + nums[r]*multipliers[m]
+	if left > right {
+		memoized[m][l] = left
+		return left
+	}
+	memoized[m][l] = right
+	return right
+}

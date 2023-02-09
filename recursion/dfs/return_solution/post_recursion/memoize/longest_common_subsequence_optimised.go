b73c69507ee6fa1c1fc5cf309a0821e47ad56bba
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2023-02-09 14:25:53 +0000

    longest common subsequence optimised

--- recursion/dfs/return_solution/post_recursion/memoize/longest_common_subsequence_optimised.go
+++ recursion/dfs/return_solution/post_recursion/memoize/longest_common_subsequence_optimised.go
@@ -1,5 +1,7 @@
 package recursion
 
+import "math"
+
 // interdependent subproblems combined for a solution -> dynamic programming
 // 2 approaches to dynamic programming:
 // 1. top-down recursion with memoization
@@ -11,16 +13,26 @@ package recursion
 // 2. recursive step
 // 3. state variables
 
-// recursive step: do we include this letter which increments subsequence by 1 but makes us jump in the
-// other sequence
 // state variable: left seq index, right seq index, don't need length because can return that - or just subsequence strings
-// question: unsure whether we should remove string to the right or not?
-// memoize? not sure yet, need something immutable maybe concat of two strings? better to do nested map
+// we can do better however
+// there are 4 scenarios
+// 1. text1 first letter included only
+// 2. text2 first letter included only
+// 3. text1 and text2 first letters both included
+// 4. neither included
+
+// in the previous recursion with memoization approach
+// * we handled 2 & 4 together - i.e. text1 not included
+// * and handled 1 & 3 together where we loop to find which of text 2 is included
+
+// an alternative solution would be to
+// * handle 3
+// * and recurse for skipping text1 i.e. 2 & 4 and then recurse for skipping text2 i.e. 1 & 4 taking max
 
 // complexity
 // time input parameters for recursive function are two strings with M possible text1 and N possible text2 which means MN
-// solving each problems takes O(N) where we search for a character in a string of length N
-// therefore time complexity is O(MN^2)
+// solving each problems takes O(1) where we no longer need to search the full string
+// therefore time complexity is O(MN)
 // space complexity = need to store solution for O(MN) problems
 func longestCommonSubsequence(text1 string, text2 string) int {
 	memoized := make(map[string]map[string]int)
@@ -32,30 +44,22 @@ func longestCommonSubsequenceRecursive(text1 string, text2 string, memoized map[
 	if len(text1) == 0 || len(text2) == 0 {
 		return 0
 	}
-	// memoize
-	if text1Map, ok := memoized[text1]; ok {
-		if longest, ok := text1Map[text2]; ok {
-			return longest
-		}
+	// memoized case
+	if memoizedLongest, ok := memoized[text1][text2]; ok {
+		return memoizedLongest
 	}
 
-	// recursive step
-	// match left
+	// recursive case
 	longest := 0
-	for i, t2 := range text2 {
-		if rune(text1[0]) == t2 {
-			longest = longestCommonSubsequenceRecursive(text1[1:], text2[i+1:], memoized) + 1
-			break
-		}
-	}
-	// do not need to match right as will do so if match not left
-	// don't match left
-	longestNotLeft := longestCommonSubsequenceRecursive(text1[1:], text2, memoized)
-	if longestNotLeft > longest {
-		longest = longestNotLeft
+	if text1[0] == text2[0] {
+		longest = longestCommonSubsequenceRecursive(text1[1:], text2[1:], memoized) + 1
+	} else {
+		longest = int(math.Max(
+			float64(longestCommonSubsequenceRecursive(text1[1:], text2, memoized)),
+			float64(longestCommonSubsequenceRecursive(text1, text2[1:], memoized)),
+		))
 	}
 	if _, ok := memoized[text1]; ok {
-		// text1Map exists just not text2
 		memoized[text1][text2] = longest
 	} else {
 		memoized[text1] = make(map[string]int)

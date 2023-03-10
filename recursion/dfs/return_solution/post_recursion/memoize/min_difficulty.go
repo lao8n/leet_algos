f1b9d894ed92665fea4430e1d6b413888b6482e4
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2023-03-10 11:27:10 +0000

    clean up shared data a bit

--- recursion/dfs/return_solution/post_recursion/memoize/min_difficulty.go
+++ recursion/dfs/return_solution/post_recursion/memoize/min_difficulty.go
@@ -23,50 +23,50 @@ func minDifficulty(jobDifficulty []int, d int) int {
 			memoizedCost[i][j] = -1
 		}
 	}
-	data := data{
+	sd := shareddata{
 		jobDifficulty: jobDifficulty,
 		memoizedCost:  memoizedCost,
 	}
-	return data.minDifficultyRecursive(0, d)
+	return sd.minDifficultyRecursive(0, d)
 }
 
-type data struct {
+type shareddata struct {
 	jobDifficulty []int
 	memoizedCost  [][]int // remaining index j & number of days d
 }
 
 // state = index of remaining jobs, num of remaining days, memoized calculation & jobDifficulty
-func (data *data) minDifficultyRecursive(j int, d int) int {
+func (sd *shareddata) minDifficultyRecursive(j int, d int) int {
 	// base cases
-	if len(data.jobDifficulty) < d {
+	if len(sd.jobDifficulty) < d {
 		return -1
 	} else if d == 1 {
 		maxCost := 0
-		for i := j; i < len(data.jobDifficulty); i++ {
-			if data.jobDifficulty[i] > maxCost {
-				maxCost = data.jobDifficulty[i]
+		for i := j; i < len(sd.jobDifficulty); i++ {
+			if sd.jobDifficulty[i] > maxCost {
+				maxCost = sd.jobDifficulty[i]
 			}
 		}
 		return maxCost
 	}
 
 	// memoized cases
-	if data.memoizedCost[j][d-1] != -1 {
-		return data.memoizedCost[j][d-1]
+	if sd.memoizedCost[j][d-1] != -1 {
+		return sd.memoizedCost[j][d-1]
 	}
 
 	// recurrence relation
 	lowestCost := math.MaxInt
 	maxCost := 0
-	for i := j; i < len(data.jobDifficulty)-d+1; i++ {
-		if data.jobDifficulty[i] > maxCost {
-			maxCost = data.jobDifficulty[i]
+	for i := j; i < len(sd.jobDifficulty)-d+1; i++ {
+		if sd.jobDifficulty[i] > maxCost {
+			maxCost = sd.jobDifficulty[i]
 		}
-		currentCost := maxCost + data.minDifficultyRecursive(i+1, d-1)
+		currentCost := maxCost + sd.minDifficultyRecursive(i+1, d-1)
 		if currentCost < lowestCost {
 			lowestCost = currentCost
 		}
 	}
-	data.memoizedCost[j][d-1] = lowestCost
+	sd.memoizedCost[j][d-1] = lowestCost
 	return lowestCost
 }

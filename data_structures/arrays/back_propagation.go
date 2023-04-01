fc81e4eb09cf55b0eb7f0fe3da9ea40003577d2b
Nicholas Drake <hellolao8n@gmail.com>
2023-04-01 17:05:03 +0100

    basis back propagation - still not correct however as don't have clean parents function

--- data_structures/arrays/back_propagation.go
+++ data_structures/arrays/back_propagation.go
@@ -1,4 +1,9 @@
+// package main
 package data_structures
+
+import (
+	"fmt"
+)
 
 // backpropagation is essentially dynamic programming where we tabulate answers backwards filling in the table
 // not sure if this solution works but the implementation is based upon the pseudo-code from bengio's deep learning
@@ -9,7 +14,7 @@ type operation struct {
 	derivative func(int) int
 }
 
-func (o operation) forward_pass(x []int, n int) int {
+func (o operation) forward_pass(x []int, n int) []int {
 	ni := len(x)
 	u := make([]int, n)
 	for i := 0; i < ni; i++ {
@@ -19,22 +24,42 @@ func (o operation) forward_pass(x []int, n int) int {
 		parents := u[:i] // really weird parent function
 		u[i] = o.function(parents)
 	}
-	return u[n-1]
+	fmt.Printf("u = %v\n", u)
+	return u
 }
 
-func (o operation) backward_pass(x []int, n int) []int {
-	ni := len(x)
+func (o operation) backward_pass(u []int, ni int, n int) []int {
 	grad_table := make([]int, n) // du_n/du_i
-	for i := 0; i < n; i++ {
+	for i := 0; i < n-1; i++ {
 		grad_table[i] = 1
 	}
-	for j := n - 1; j >= 0; j-- {
+	grad_table[n-1] = o.derivative(u[n-1]) // this isn't right
+	for j := ni - 1; j >= 0; j-- {
 		sum_partials := 0
-		for i := j; i < n; i++ {
-			// all the i where j is the parent of i
-			sum_partials = grad_table[i] * o.derivative(x[i]) // not sure if this is correct? x[i] should be du_i/du_j
+		for i := ni; i < n; i++ { // j's children are all the i that follow
+			sum_partials = grad_table[i] * o.derivative(u[j]) // not sure if this is correct? u[i] should be du_i/du_j
+			fmt.Println(j, i, sum_partials)
 		}
 		grad_table[j] = sum_partials
 	}
 	return grad_table[:ni]
+}
+
+func main() {
+	operation := operation{
+		function: func(x []int) int {
+			result := 0
+			for i := 0; i < len(x); i++ {
+				result += x[i] * x[i]
+			}
+			return result
+		},
+		derivative: func(x int) int {
+			return 2 * x
+		},
+	}
+	x := []int{3, 2, 4}
+	u := operation.forward_pass(x, 4)
+	gradients := operation.backward_pass(u, 3, 4)
+	fmt.Println(gradients)
 }

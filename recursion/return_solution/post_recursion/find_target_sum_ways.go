49a72f4627aea1255461ed83b29b8dafccb5d28e
Nicholas Drake <hellolao8n@gmail.com>
2023-06-03 01:15:55 +0100

    find target sum ways by recursion

---
+++ recursion/return_solution/post_recursion/find_target_sum_ways.go
@@ -0,0 +1,18 @@
+package recursion
+
+// dfs by 1. recursion or 2. stack
+func findTargetSumWays(nums []int, target int) int {
+	// base case
+	if len(nums) == 0 {
+		if target == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	// recursive case
+	positiveWays := findTargetSumWays(nums[1:], target-nums[0]) // adding reduces target
+	negativeWays := findTargetSumWays(nums[1:], target+nums[0]) // subtracting increases target
+	return positiveWays + negativeWays
+}

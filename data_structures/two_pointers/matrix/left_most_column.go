8d1fe747b46dfd6747540556f80ea5e586b87968
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2023-10-16 14:35:33 +0100

    optimised versions of left most column with easy walk across and diagonal sort with counting sort

---
+++ data_structures/two_pointers/matrix/left_most_column.go
@@ -0,0 +1,18 @@
+package data_structures
+
+func leftMostColumnWithOne(binaryMatrix BinaryMatrix) int {
+	dimensions := binaryMatrix.Dimensions()
+	m, n := dimensions[0], dimensions[1]
+	i, j := 0, n-1
+	for i < m && j >= 0 {
+		if binaryMatrix.Get(i, j) == 0 {
+			i++
+		} else {
+			j--
+		}
+	}
+	if j == n-1 {
+		return -1
+	}
+	return j + 1
+}

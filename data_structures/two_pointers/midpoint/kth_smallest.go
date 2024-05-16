e05087f503c8351ab1b907e742af62f70b2d5caf
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2024-05-16 18:06:40 +0100

    binary search over a 2d matrix

---
+++ data_structures/two_pointers/midpoint/kth_smallest.go
@@ -0,0 +1,26 @@
+package data_structures
+
+import "sort"
+
+func kthSmallest(matrix [][]int, k int) int {
+	i, j := matrix[0][0], matrix[len(matrix)-1][len(matrix[0])-1]
+	for i < j {
+		mid := i + (j-i)/2
+		if smallerCount(matrix, mid) < k {
+			i = mid + 1
+		} else {
+			j = mid
+		}
+	}
+	return i
+}
+
+func smallerCount(matrix [][]int, k int) int {
+	ret := 0
+	for _, v := range matrix {
+		ret = ret + sort.Search(len(v), func(i int) bool {
+			return v[i] > k
+		})
+	}
+	return ret
+}

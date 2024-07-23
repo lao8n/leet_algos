4ac2c1cbc35013792f989a7e130ef9e3cb2f0f09
Nicholas Drake <hellolao8n@gmail.com>
2024-07-23 23:10:19 +0100

    channel implementation of bounded blocking queue

---
+++ concurrency/data_structures/channels/bounded_blocking_queue.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// package data_structures
+type BoundedBlockingQueue struct {
+	queue chan int
+}
+
+func Constructor(capacity int) BoundedBlockingQueue {
+	return BoundedBlockingQueue{
+		queue: make(chan int, capacity),
+	}
+}
+
+func (q *BoundedBlockingQueue) Enqueue(element int) {
+	q.queue <- element // block if channel is full
+	fmt.Println("enqueue:", element)
+}
+
+func (q *BoundedBlockingQueue) Dequeue() int {
+	element := <-q.queue
+	fmt.Println("dequeue: ", element)
+	return element // block if channel is empty
+}
+
+func (q *BoundedBlockingQueue) Size() int {
+	return len(q.queue)
+}
+
+func main() {
+	tests := []struct {
+		name         string
+		threads      string
+		capacity     int
+		functions    []string
+		args         [][]int
+		expected     []int
+		expectedSize int
+	}{
+		{
+			name:         "example 1",
+			threads:      "one_each",
+			capacity:     2,
+			functions:    []string{"enqueue", "dequeue", "dequeue", "enqueue", "enqueue", "enqueue", "enqueue", "dequeue"},
+			args:         [][]int{{1}, nil, nil, {0}, {2}, {3}, {4}, nil},
+			expected:     []int{1, 0, 2},
+			expectedSize: 2,
+		},
+		{
+			name:         "example 2",
+			threads:      "one_per",
+			capacity:     3,
+			functions:    []string{"enqueue", "enqueue", "enqueue", "dequeue", "dequeue", "dequeue", "enqueue"},
+			args:         [][]int{{1}, {0}, {2}, nil, nil, nil, {3}},
+			expected:     []int{1, 0, 2},
+			expectedSize: 1,
+		},
+	}
+
+	for _, test := range tests {
+		fmt.Println(test.name)
+		queue := Constructor(test.capacity)
+		solution := []int{}
+		if test.threads == "one_each" {
+			go func() {
+				for i, function := range test.functions {
+					if function == "enqueue" {
+						if test.args[i] != nil {
+							queue.Enqueue(test.args[i][0])
+						}
+					}
+				}
+			}()
+			go func() {
+				for _, function := range test.functions {
+					if function == "dequeue" {
+						solution = append(solution, queue.Dequeue())
+					}
+				}
+			}()
+		} else if test.threads == "one_per" {
+			for i, function := range test.functions {
+				if function == "enqueue" {
+					if test.args[i] != nil {
+						j := i
+						go func() {
+							queue.Enqueue(test.args[j][0])
+						}()
+					}
+					time.Sleep(1 * time.Millisecond)
+				} else if function == "dequeue" {
+					go func() {
+						solution = append(solution, queue.Dequeue())
+					}()
+				}
+			}
+		}
+		time.Sleep(1 * time.Second)
+		if len(solution) != len(test.expected) {
+			fmt.Printf("different lengths %v %v\n", solution, test.expected)
+		} else {
+			for i := 0; i < len(test.expected); i++ {
+				if solution[i] != test.expected[i] {
+					fmt.Printf("different %v %v\n", solution, test.expected)
+					break
+				}
+				if i == len(test.expected)-1 {
+					fmt.Println("test passed")
+				}
+			}
+		}
+		if test.expectedSize != queue.Size() {
+			fmt.Printf("queue is %v size not %v", queue.Size(), test.expectedSize)
+		} else {
+			fmt.Println("queue size test passes")
+		}
+	}
+}

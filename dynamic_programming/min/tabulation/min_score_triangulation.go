1bd4a975f4cd64e944217e9499b4270a32656521
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2024-08-01 17:24:56 +0100

    this code works

--- dynamic_programming/min/tabulation/min_score_triangulation.go
+++ dynamic_programming/min/tabulation/min_score_triangulation.go
@@ -7,16 +7,13 @@ func minScoreTriangulation(values []int) int {
 	for i := 0; i < len(dp); i++ {
 		dp[i] = make([]int, len(values)+1)
 	}
-
 	return dfs(values, 0, len(values)-1, dp)
 }
 
 func dfs(values []int, i, j int, dp [][]int) int {
-	// base case
-	if j-1 < 2 {
+	if j-i < 2 {
 		return 0
 	}
-	// memoized case
 	if dp[i][j] != 0 {
 		return dp[i][j]
 	}
@@ -25,15 +22,12 @@ func dfs(values []int, i, j int, dp [][]int) int {
 		dp[i][j] = res
 		return res
 	}
-	// recursive case
+
 	lowestScore := math.MaxInt
 	for k := i + 1; k < j; k++ {
 		ll := dfs(values, i, k, dp)
 		rr := dfs(values, k, j, dp)
-		score := ll + rr + values[i]*values[j]*values[k]
-		if score < lowestScore {
-			lowestScore = score
-		}
+		lowestScore = min(lowestScore, ll+rr+values[i]*values[j]*values[k])
 	}
 	dp[i][j] = lowestScore
 	return lowestScore

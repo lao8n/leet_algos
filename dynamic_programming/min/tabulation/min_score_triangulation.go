855989d36c7a33c166711499f936aac836ba3906
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2024-08-01 17:35:57 +0100

    add notes and picture

--- dynamic_programming/min/tabulation/min_score_triangulation.go
+++ dynamic_programming/min/tabulation/min_score_triangulation.go
@@ -1,16 +1,22 @@
 package data_structures
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func minScoreTriangulation(values []int) int {
-	dp := make([][]int, len(values)+1)
+	dp := make([][]int, len(values))
 	for i := 0; i < len(dp); i++ {
-		dp[i] = make([]int, len(values)+1)
+		dp[i] = make([]int, len(values))
 	}
-	return dfs(values, 0, len(values)-1, dp)
+	x := dfs(values, 0, len(values)-1, dp) // i & j are two neighbours
+	fmt.Println(dp)
+	return x
 }
 
 func dfs(values []int, i, j int, dp [][]int) int {
+	// base case
 	if j-i < 2 {
 		return 0
 	}
@@ -22,9 +28,10 @@ func dfs(values []int, i, j int, dp [][]int) int {
 		dp[i][j] = res
 		return res
 	}
-
+	// recursive case
 	lowestScore := math.MaxInt
 	for k := i + 1; k < j; k++ {
+		// try having a triangle with k
 		ll := dfs(values, i, k, dp)
 		rr := dfs(values, k, j, dp)
 		lowestScore = min(lowestScore, ll+rr+values[i]*values[j]*values[k])
